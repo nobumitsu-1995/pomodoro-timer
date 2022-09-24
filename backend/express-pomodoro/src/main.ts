5e40522089ede41f397fcac110abc09c16ca0b8c
nobumitsu_masugata <ys.training220208@gmail.com>
2022-09-25 02:05:03 +0900

    create: アプリケーション雛形の作成

---
+++ backend/express-pomodoro/src/main.ts
@@ -0,0 +1,49 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { config } from 'dotenv'
+
+// 変数の定義
+const app = express()
+config()
+const isTest = process.env.NODE_ENV === 'test'
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+const client = process.env.CLIENT_URL || 'http://localhost:8080'
+const dbPath = isTest
+  ? 'mongodb://localhost:27017/pomodoro_test' // test環境
+  : isDev
+  ? 'mongodb://mongo-pomodoro:27017/pomodoro_dev' // dev環境
+  : 'mongodb://mongo-pomodoro:27017/pomodoro' // 本番環境
+const port = isTest ? 3001 : 3000
+
+// applicationの基本設定
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(
+  cors({
+    origin: client,
+    credentials: true,
+  })
+)
+
+// データベース設定
+mongoose.Promise = global.Promise
+mongoose.connect(dbPath)
+const db = mongoose.connection
+db.once('open', () => {
+  console.log(`Successfully connected to ${dbPath} useing Mongoose`)
+})
+
+// サーバーの起動
+app.listen(port, () => {
+  console.log(
+    `the server is runnning at 
+    PORT: ${port},
+    ENV: ${process.env.NODE_ENV},
+    CLIENT: ${client},
+    DB: ${dbPath}`
+  )
+})
+
+module.exports = app
---
+++ frontend/react-pomodoro/src/App.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import App from './App'
+
+export default {
+  title: 'Example/App',
+  component: App,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof App>
+
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  primary: true,
+  label: 'App',
+}
