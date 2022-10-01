640fe31e5d87ecc8686b2e2c8fabd664f6306788
nobumitsu_masugata <ys.training220208@gmail.com>
2022-10-01 22:10:01 +0900

    edit: レイアウト調整(frontend1)

--- frontend/react-pomodoro/src/App.tsx
+++ frontend/react-pomodoro/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ModalBody } from './components/organisms'
 import { Header, TimerBlock } from './components/templates'
 import { ModalProvider } from './lib/functions/ModalContext'
@@ -7,10 +8,16 @@ const App: React.FC = () => {
   return (
     <ModalProvider>
       <Header />
-      <TimerBlock />
-      <ModalBody />
+      <StyledMain>
+        <TimerBlock />
+        <ModalBody />
+      </StyledMain>
     </ModalProvider>
   )
 }
 
 export default App
+
+const StyledMain = styled.main`
+  margin-top: 50px;
+`
