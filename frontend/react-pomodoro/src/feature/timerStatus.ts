0e5a8811863b467a9a2d70935f5975f8eb07b460
nobumitsu_masugata <ys.training220208@gmail.com>
2022-10-06 23:24:50 +0900

    edit: timerStatusをreduxでの管理に変更(frontend1)

---
+++ frontend/react-pomodoro/src/feature/timerStatus.ts
@@ -0,0 +1,81 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+/** timerStatusのfeatureの型 */
+export type TimerStatusType = {
+  /** タイマーは稼働中 */
+  isRunning: boolean
+  /** タイマーは一時停止中 */
+  isPause: boolean
+  /** タイマーは稼働中で休憩時間 */
+  isRest: boolean
+  /** タイマーの残り時間 */
+  leftTime: number
+}
+
+/** timerStatusのfeatureの初期値 */
+const initialState: TimerStatusType = {
+  isRunning: false,
+  isPause: false,
+  isRest: false,
+  leftTime: 60 * 25,
+}
+
+/** timerStatusのfeatureのSlice */
+export const timerStatusSlice = createSlice({
+  name: 'timerStatus',
+  initialState,
+  reducers: {
+    updateIsRunning: (state) => {
+      state.isRunning = !state.isRunning
+    },
+    updateIsPause: (state) => {
+      state.isPause = !state.isPause
+    },
+    updateIsRest: (state) => {
+      state.isRest = !state.isRest
+    },
+    initTimerStatus: (state) => {
+      state.isRunning = false
+      state.isPause = false
+      state.isRest = false
+    },
+    workFinish: (state) => {
+      state.isRunning = true
+      state.isPause = false
+      state.isRest = true
+    },
+    restFinish: (state) => {
+      state.isRunning = true
+      state.isPause = false
+      state.isRest = false
+    },
+    updatePlay: (state) => {
+      state.isRunning = true
+      state.isPause = false
+    },
+    updatePause: (state) => {
+      state.isPause = true
+    },
+    setLeftTime: (state, action: PayloadAction<number>) => {
+      state.leftTime = action.payload
+    },
+    passLeftTime: (state) => {
+      state.leftTime -= 1
+    },
+  },
+})
+
+const { actions, reducer } = timerStatusSlice
+export const {
+  updateIsRunning,
+  updateIsPause,
+  updateIsRest,
+  initTimerStatus,
+  workFinish,
+  restFinish,
+  updatePlay,
+  updatePause,
+  setLeftTime,
+  passLeftTime,
+} = actions
+export const timerStatusReducer = reducer
