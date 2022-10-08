2ac470bd33cc9ccea8572a8c13dedfafc41d6174
nobumitsu_masugata <ys.training220208@gmail.com>
2022-10-08 20:47:47 +0900

    edit: noticesのルート、コントローラーを整備(backend1)

--- backend/express-pomodoro/src/controllers/noticesController.ts
+++ backend/express-pomodoro/src/controllers/noticesController.ts
@@ -18,3 +18,88 @@ export const getNotices = (req: Request, res: Response, next: NextFunction) => {
       next(e)
     })
 }
+
+export const getNotice = (req: Request, res: Response, next: NextFunction) => {
+  const noticeId = req.params.id
+  Notice.findById(noticeId)
+    .then((notice) => {
+      if (notice) {
+        res.render('notices/show', notice)
+      }
+      next({ message: "Notice's data is not find!" })
+    })
+    .catch((e) => {
+      next(e)
+    })
+}
+
+export const renderNewPage = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.render('notices/new')
+}
+
+export const createNotice = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const noticeParams = getNoticeParams(req.body)
+  Notice.create(noticeParams)
+    .then((notice) => {
+      res.render('notices/show', notice)
+    })
+    .catch((e) => {
+      next(e)
+    })
+}
+
+export const renderEditPage = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const noticeId = req.params.id
+  Notice.findById(noticeId)
+    .then((notice) => {
+      if (notice) {
+        res.render('notices/edit', notice)
+      }
+      next({ message: "Notice's data is not find!" })
+    })
+    .catch((e) => {
+      next(e)
+    })
+}
+
+export const updateNotice = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const noticeId = req.params.id
+  const noticeParams = getNoticeParams(req.body)
+  Notice.findByIdAndUpdate(noticeId, { $set: noticeParams }, { new: true })
+    .then((notice) => {
+      if (notice) {
+        res.render('notices/show', notice)
+      }
+      next({ message: "Notice's data is not find!" })
+    })
+    .catch((e) => {
+      next(e)
+    })
+}
+
+export const deleteNotice = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const noticeId = req.params.id
+  Notice.findByIdAndDelete(noticeId).then(() => {
+    res.redirect('/notices')
+  })
+}
--- backend/express-pomodoro/src/routes/notice.ts
+++ backend/express-pomodoro/src/routes/notice.ts
@@ -1,14 +1,21 @@
 import express from 'express'
-import { getNotices } from '../controllers/noticesController'
+import {
+  createNotice,
+  getNotice,
+  getNotices,
+  renderEditPage,
+  renderNewPage,
+  updateNotice,
+} from '../controllers/noticesController'
 
 const router = express.Router()
 
 router.get('/', getNotices)
-router.get('/:id')
-router.get('/new')
-router.post('/create')
-router.get('/:id/edit')
-router.patch('/:id/update')
+router.get('/:id/show', getNotice)
+router.get('/new', renderNewPage)
+router.post('/create', createNotice)
+router.get('/:id/edit', renderEditPage)
+router.patch('/:id/update', updateNotice)
 router.delete('/:id/delete')
 
 export default router
