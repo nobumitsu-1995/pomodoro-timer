84773a880f2c3a45875d032556930715fc0b4277
nobumitsu_masugata <ys.training220208@gmail.com>
2022-11-06 15:32:21 +0900

    topページの追加(backend1)

--- backend/express-pomodoro/src/routes/index.ts
+++ backend/express-pomodoro/src/routes/index.ts
@@ -7,9 +7,12 @@ const router = express.Router()
 
 router.use('/api/v1', apiRouterV1)
 router.use(
-  '/',
+  '/notices',
   passport.authenticate('basic', { session: false }),
   noticeRouter
 )
+router.use('/', (req, res) => {
+  res.render('index')
+})
 
 export default router
