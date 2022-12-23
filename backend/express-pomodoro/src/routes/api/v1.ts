7dfe995fe06b974e6abc32a75b1722d215d9aa59
nobumitsu_masugata <ys.training220208@gmail.com>
2022-12-23 21:54:04 +0900

    add: routeの追加(backend3)

--- backend/express-pomodoro/src/routes/api/v1.ts
+++ backend/express-pomodoro/src/routes/api/v1.ts
@@ -7,26 +7,44 @@ import {
   getCustumConfigs,
   initializeCustumConfig,
   updateCustumConfig,
-  validator,
+  validator as custumConfigValidator,
 } from '../../controllers/api/v1/custumConfigController'
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  updateTask,
+  validator as taskValidator,
+} from '../../controllers/api/v1/tasksController'
+import {
+  createWorkTime,
+  deleteWorkTime,
+  getWorkTime,
+  getWorkTimes,
+  updateWorkTime,
+  validator as workTimeValidator,
+} from '../../controllers/api/v1/workTimesController'
 import { getNotices } from '../../controllers/api/v1/noticesController'
 
 const router = express.Router()
 
+//notices
 router.get('/notices', getNotices)
 
+//custum_config
 router.get('/custum_config', authenticateJWT, getCustumConfigs)
 router.get('/custum_config/:id/show', authenticateJWT, getCustumConfig)
 router.post(
   '/custum_config/create',
   authenticateJWT,
-  validator,
+  custumConfigValidator,
   createCustumConfig
 )
 router.patch(
   '/custum_config/:id/update',
   authenticateJWT,
-  validator,
+  custumConfigValidator,
   updateCustumConfig
 )
 router.delete('/custum_config/:id/delete', authenticateJWT, deleteCustumConfig)
@@ -35,5 +53,29 @@ router.post(
   authenticateJWT,
   initializeCustumConfig
 )
+
+// task
+router.get('/task', authenticateJWT, getTasks)
+router.get('/task/:id/show', authenticateJWT, getTask)
+router.post('/task/create', authenticateJWT, taskValidator, createTask)
+router.patch('/task/:id/update', authenticateJWT, taskValidator, updateTask)
+router.delete('/task/:id/delete', authenticateJWT, deleteTask)
+
+// tworkTime
+router.get('/work_time', authenticateJWT, getWorkTimes)
+router.get('/work_time/:id/show', authenticateJWT, getWorkTime)
+router.post(
+  '/work_time/create',
+  authenticateJWT,
+  workTimeValidator,
+  createWorkTime
+)
+router.patch(
+  '/work_time/:id/update',
+  authenticateJWT,
+  workTimeValidator,
+  updateWorkTime
+)
+router.delete('/work_time/:id/delete', authenticateJWT, deleteWorkTime)
 
 export default router
