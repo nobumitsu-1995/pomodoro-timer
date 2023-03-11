707fb0f2090dd5b6164b9a160faa421396bd1277
nobumitsu_masugata <ys.training220208@gmail.com>
2023-03-11 20:14:12 +0900

    add: atomsにsnapshotテストを追加

---
+++ frontend/react-pomodoro/src/components/atoms/Input/Input.test.tsx
@@ -0,0 +1,19 @@
+import * as stories from './Input.stories'
+import { composeStories } from '@storybook/react'
+import { render } from '@testing-library/react'
+import Theme from 'src/assets/styles/Theme'
+
+describe('snapshot', () => {
+  Object.entries(composeStories(stories)).forEach(([name, Component]) => {
+    describe(name, () => {
+      it('should render correctly', () => {
+        const tree = render(
+          <Theme>
+            <Component {...Component.args} />
+          </Theme>
+        ).asFragment()
+        expect(tree).toMatchSnapshot()
+      })
+    })
+  })
+})
