5d16c895930d474c55990df9411c5a4472044362
nobumitsu_masugata <ys.training220208@gmail.com>
2023-04-02 21:58:13 +0900

    add: SpConsoleコンポーネントの作成

---
+++ frontend/react-pomodoro/src/components/organisms/SpConsole/Container.tsx
@@ -0,0 +1,52 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React, { useState } from 'react'
+import { Notice } from '..'
+import UserAchievement from '../UserAchievement/Container'
+import Presenter from './Presenter'
+
+const Container = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [content, setContent] = useState<string>('')
+  const { isAuthenticated } = useAuth0()
+
+  const touchButton = (id: string) => {
+    if (id === content) {
+      return setIsOpen(!isOpen)
+    }
+    if (isOpen) {
+      return setContent(id)
+    }
+    setContent(id)
+    setIsOpen(!isOpen)
+  }
+
+  const CONTENT_LIST = [
+    {
+      id: 'notice',
+      content: <Notice />,
+    },
+    {
+      id: 'help',
+      content: <Notice />,
+    },
+    {
+      id: 'config',
+      content: <Notice />,
+    },
+    {
+      id: 'user',
+      content: <UserAchievement />,
+    },
+  ]
+
+  return (
+    <Presenter
+      isLoggedIn={isAuthenticated}
+      isOpen={isOpen}
+      content={CONTENT_LIST.find((v) => v.id === content)?.content}
+      touchButton={touchButton}
+    />
+  )
+}
+
+export default Container
